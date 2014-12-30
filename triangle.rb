d73f4259b6b74ae39136f6e2b6c3682449e6de42
Sean Carroll <sean.carroll@mac.com>
2014-12-30 16:32:21 -0600

    completed about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	if (a <= 0) || (b <= 0) || (c <= 0)
+		raise TriangleError
+	elsif (a+b<=c) || (b+c<=a) || (a+c<=b)
+		raise TriangleError
+	end
 	if (a==b) && (a==c)
 		return :equilateral
 	elsif (a==b) || (a==c) || (b==c)
